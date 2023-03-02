f7758d494988c88142930aff0af2a9caecad0996
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2023-03-02 22:07:19 +0200

    Final Commit

--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -1,18 +1,19 @@
 const { User } = require('../models/user');
-const { Unathorized } = require('http-errors');
+const { Unauthorized, BadRequest } = require('http-errors');
 const jwt = require('jsonwebtoken');
 
 const authCheck = async (req, res, next) => {
   const { authorization = '' } = req.headers;
   const [bearer, token] = authorization.split(' ');
   try {
-    if (bearer !== 'Bearer') {
-      throw new Unathorized('Not authorized');
+    if (bearer !== 'Bearer' || !token) {
+
+      throw new Error('Not authorized');
     }
     const { id } = jwt.verify(token, process.env.SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token) {
-      throw new Unathorized('Not authorized');
+      throw new Error('Not authorized');
     }
     req.user = user;
     next();
